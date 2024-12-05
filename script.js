e01f2619d2a79133e599ea2d0070589c1b24acd5
João Estevam <joaoestevam823@gmail.com>
2024-12-05 16:18:42 -0300

    Completo

--- script.js
+++ script.js
@@ -3,9 +3,17 @@ function insert(num){
     document.getElementById('resultado').innerHTML = numero + num
 }
 function calcular(){
-    document.getSelection
+    var resultado = document.getElementById('resultado').innerHTML
+    if(resultado){
+        document.getElementById('resultado').innerHTML = eval(resultado)
+    }
 }
 
 function clean(){
     document.getElementById('resultado').innerHTML = ''
+}
+
+function back(){
+    var resultado = document.getElementById('resultado').innerHTML
+    document.getElementById('resultado').innerHTML = resultado.substring(0,resultado.length -1)
 }
