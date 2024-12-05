2a1d8f86c7baca74836e44cd1d904cd3275b0008
João Estevam <joaoestevam823@gmail.com>
2024-12-05 16:05:56 -0300

    Style and script

---
+++ script.js
@@ -0,0 +1,11 @@
+function insert(num){
+    var numero = document.getElementById('resultado').innerHTML
+    document.getElementById('resultado').innerHTML = numero + num
+}
+function calcular(){
+    document.getSelection
+}
+
+function clean(){
+    document.getElementById('resultado').innerHTML = ''
+}
